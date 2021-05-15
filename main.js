207a0ab5eba9153363585d69c65f0537ec8bc4ee
M_abedalnabi <abedalnabi96@gmail.com>
2021-05-15 21:52:49 +0300

    deleteArticlesByAuthor

--- main.js
+++ main.js
@@ -132,6 +132,27 @@ const deleteArticleById = (req, res, next) => {
 
 app.delete("/articles/:id", deleteArticleById);
 
+const deleteArticlesByAuthor = (req, res, next) => {
+  let deleteAuthor = req.body.author;
+  articles.forEach((ele, i) => {
+    if (ele.author === deleteAuthor) {
+      articles.splice(i, 1);
+      res.status(410);
+      res.json({
+        success: true,
+        message: ` Success delete all the articles for the author => ${deleteAuthor}`,
+      });
+      next();
+    } else {
+      res.status(404);
+      res.json("No author have this name");
+      next();
+    }
+  });
+};
+
+app.delete("/articles", deleteArticlesByAuthor);
+
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
