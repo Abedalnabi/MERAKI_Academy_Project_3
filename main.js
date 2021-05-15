8cebb56aacfb183e9e9b4bb55c65dc9376722eb0
M_abedalnabi <abedalnabi96@gmail.com>
2021-05-15 20:42:21 +0300

    updateAnArticleById

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { json } = require("express");
 const express = require("express");
 const { uuid } = require("uuidv4");
+//ORR
+// const uuid = require("uuidv4");
+// console.log(uuid.uuid());
 
 const app = express();
 const PORT = 5000;
-
 app.use(express.json());
 
 const articles = [
@@ -65,10 +67,29 @@ const createNewArticle = (req, res, next) => {
   articles.push(newArticle);
   res.status(201);
   res.json(newArticle);
+  next();
 };
 
 app.post("/articles", createNewArticle);
 
+const updateAnArticleById = (req, res, next) => {
+  let id = JSON.parse(req.parse.id);
+  let index;
+  let found = articles.find((ele, i) => {
+    index = i;
+    return id === ele.id;
+  });
+  if (found) {
+    articles[index] = {
+      id: id,
+      title: req.body.title,
+      description: req.body.description,
+      author: req.body.author,
+    };
+  }
+};
+
+app.put("/articles/:id", updateAnArticleById);
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
